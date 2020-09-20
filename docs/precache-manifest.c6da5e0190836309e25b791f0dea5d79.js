0401dcbf888ec5fd86014c8d48a52c6999dcda1c
jays29 <juangnr@gmail.com>
2020-09-19 22:01:34 -0300

    First build

---
+++ docs/precache-manifest.c6da5e0190836309e25b791f0dea5d79.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd3ec0e56b693a0f4a64f2b5687769fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "37fbbda81bd820e1f9b4",
+    "url": "/static/css/main.6409d92d.chunk.css"
+  },
+  {
+    "revision": "e84f2dab5cdc80c3798a",
+    "url": "/static/js/2.6745d15c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6745d15c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37fbbda81bd820e1f9b4",
+    "url": "/static/js/main.47332c50.chunk.js"
+  },
+  {
+    "revision": "e6076f868cd9d279e56c",
+    "url": "/static/js/runtime-main.073cbf64.js"
+  }
+]);
